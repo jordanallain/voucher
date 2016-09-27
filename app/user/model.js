8677d9ce3fbcdc8ceb0f058fa2fe71e3eb108ebb
jordanallain <jallain@gmail.com>
2016-09-27 01:03:19 -0400

    Add instruments to user template
    Attempt to display data for instruments on user template.

--- app/user/model.js
+++ app/user/model.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  bio: DS.attr('string')
+  bio: DS.attr('string'),
+  instruments: DS.hasMany('instrument')
 });
