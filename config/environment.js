74b8bcb37d9b2faaa2b701e0d206c8f5e2e5600b
jordanallain <jallain@gmail.com>
2016-09-28 11:17:08 -0400

    Comment out local host api
    Comment out the localhost line from environment.js

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    apiHost: 'http://localhost:3000',
+    // apiHost: 'http://localhost:3000',
     modulePrefix: 'voucher',
     environment: environment,
     baseURL: '/',
