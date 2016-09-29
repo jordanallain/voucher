dfa173c48b46c65c092ab1dc4b7bb666bb9d9182
jordanallain <jallain@gmail.com>
2016-09-29 13:04:22 -0400

    Remove console logs and comments
    Remove the unnecessary comments and console logs from
    testing.

--- app/components/user-instrument/component.js
+++ app/components/user-instrument/component.js
@@ -6,13 +6,8 @@ export default Ember.Component.extend({
   actions: {
     deleteInstrument() {
       this.sendAction('deleteInstrument', this.get('userInstrument'));
-      // console.log(this.get('userInstrument'));
     },
     updateStyles() {
-      let data = this.get('userInstrument');
-      console.log(data);
-      console.log(data.instrument);
-      console.log(data.styles);
       this.sendAction('updateStyles', this.get('userInstrument'));
     }
   }
