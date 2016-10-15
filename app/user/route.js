a0c00aab40cbf3363a899b83706c21f53b77cdd5
jordanallain <jallain@gmail.com>
2016-10-15 18:47:02 -0400

    Display user email and instruments
    Display the users email and instruments on their profile view.

---
+++ app/user/route.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model (params) {
+    return this.get('store').findRecord('user', params.user_id);
+  },
+
+  actions: {
+
+  },
+});
